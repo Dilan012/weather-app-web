768c5af1d4b016631c0c7e6539cdf7b74a84d0dd
Dilan Madhusankha <dilanmadusanka5555@gmail.com>
2024-08-15 21:53:47 +0530

    change the navigation bar height and nav options

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,13 +2,15 @@ import './navbar.css'
 function Navbar(){
 
     return(
-        <div>
-        <h1 className='title'>GalaxyWheather</h1>
-            <ul className="navbar-ul">
-                <li>Home</li>
-                <li>Contact us</li>
-                <li>About us</li>
-            </ul>
+        <div className='nav-container'>
+            <div className='nav-title-container'><h1 className='title'>GalaxyWheather</h1></div>
+            <div className='nav-option-container'>
+                <ul className="navbar-ul">
+                    <li>Home</li>
+                    <li>Contact us</li>
+                    <li>About us</li>
+                </ul>
+            </div>
         </div>
     )
 }
